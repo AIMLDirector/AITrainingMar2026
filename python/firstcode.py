373c2cfd9ea4f96e06032b6be2c8b06cad9a32e8
premkumar <premkumar.unix@gmail.com>
2026-03-10 07:19:54 +0530

    added python folder

---
+++ python/firstcode.py
@@ -0,0 +1,13 @@
+a = input("what is your name? ")
+b = input("what is your age? ")
+
+print("Hello " + a + ", you are " + b + " years old.")
+print("Hello " , a , ", you are " , b , " years old.")
+print(f"Hello {a}, you are {b} years old.")  # f-string, available in Python 3.6 and later
+print("Hello {}, you are {} years old.".format(a, b))
+print("Hello %s, you are %s years old." % (a, b))
+
+
+
+
+
